8d6ab0fe51319639d7dea9d068be6f1c9e8943ae
Ta Duc Huy (ted) <tdh512194@gmail.com>
2025-03-27 17:41:09 +1030

    fix project responsibilities

--- prompt.py
+++ prompt.py
@@ -23,7 +23,7 @@ Education			education	list of dict
 Projects			projects	list of dict
 	Timeline	From - To	project_timeline	tuple of int
 	Project name	Project name	project_name	string
-  Responsibilities	Responsibilities, achievements done, written in bullet points, copy from the resume project_responsibilities	string
+  Responsibilities	Responsibilities, achievements done, written in bullet points, copy from the resume project_responsibilities	list of string
   Technologies	tools, frameworks, technologies used in the projects technologies	string
 	Descriptions	Description of the project	project_description	string
 Certifications  N/A list of achieved certifications certifications  string
